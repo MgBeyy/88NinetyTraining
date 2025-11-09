b21d496d124e9fa7bc7f592943d5157f8b2ac11b
zaid <zaidalmoughrabi@gmail.com>
2025-11-09 12:25:22 +0300

    Add entity classes for Assignment, Comment, Course, Grade, Syllabus, and User

---
+++ Entity Framework/EFCoreWithConsoleApp/Entities/Comment.cs
@@ -0,0 +1,16 @@
+﻿namespace EFCoreWithConsoleApp.Entities
+{
+    public class Comment
+    {
+        public int Id { get; set; }
+        public int AssignmentId { get; set; }
+        public int CreatedByUserId { get; set; }
+        public DateTime CreatedDate { get; set; }
+        public string CommentContent { get; set; }
+
+
+        public Assignment Assignment { get; set; }
+        public User CreatedByUser { get; set; }
+
+    }
+}
